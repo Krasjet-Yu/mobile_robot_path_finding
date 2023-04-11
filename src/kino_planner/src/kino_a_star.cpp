2cacd738018f71072c907bd7524b968d89e07c6b
Krasjet_Yu <krasjet.ziping@gmail.com>
2023-04-11 20:19:56 +0800

    obvp

--- src/kino_planner/src/kino_a_star.cpp
+++ src/kino_planner/src/kino_a_star.cpp
@@ -209,17 +209,66 @@ bool KinodynamicAstar::computeShotTraj(Eigen::VectorXd state1, Eigen::VectorXd s
 std::vector<double> KinodynamicAstar::cubic(double a, double b, double c, double d) {
   std::vector<double> dts;
 
-  // TODO
-  dts.push_back(0.0);
+  double a2 = b / a;
+  double a1 = c / a;
+  double a0 = d / a;
 
-  return dts;
+  double Q = (3 * a1 - a2 * a2) / 9;
+  double R = (9 * a1 * a2 - 27 * a0 - 2 * a2 * a2 * a2) / 54;
+  double D = Q * Q * Q + R * R;
+  if (D > 0) {
+    double S = std::cbrt(R + sqrt(D));
+    double T = std::cbrt(R - sqrt(D));
+    dts.push_back(-a2 / 3 + (S + T));
+    return dts;
+  } else if (D == 0) {
+    double S = std::cbrt(R);
+    dts.push_back(-a2 / 3 + S + S);
+    dts.push_back(-a2 / 3 - S);
+    return dts;
+  } else {
+    double theta = acos(R / sqrt(-Q * Q * Q));
+    dts.push_back(2 * sqrt(-Q) * cos(theta / 3) - a2 / 3);
+    dts.push_back(2 * sqrt(-Q) * cos((theta + 2 * M_PI) / 3) - a2 / 3);
+    dts.push_back(2 * sqrt(-Q) * cos((theta + 4 * M_PI) / 3) - a2 / 3);
+    return dts;
+  }
 }
 
 std::vector<double> KinodynamicAstar::quartic(double a, double b, double c, double d, double e) {
+  
+  // ref: https://zhuanlan.zhihu.com/p/491057733#ref_2
+
   vector<double> dts;
 
-  // TODO
-  dts.push_back(0.0);
+  double a3 = b / a;
+  double a2 = c / a;
+  double a1 = d / a;
+  double a0 = e / a;
+
+  std::vector<double> ys = cubic(1, -a2, a1 * a3 - 4 * a0, 4 * a2 * a0 - a1 * a1 - a3 * a3 * a0);
+  double y1 = ys.front();
+  double r = a3 * a3 / 4 - a2 + y1;
+  if (r < 0) return dts;
+
+  double R = sqrt(r);
+  double D, E;
+  if (R != 0) {
+    D = sqrt(0.75 * a3 * a3 - R * R - 2 * a2 + 0.25 * (4 * a3 * a2 - 8 * a1 - a3 * a3 * a3) / R);
+    E = sqrt(0.75 * a3 * a3 - R * R - 2 * a2 - 0.25 * (4 * a3 * a2 - 8 * a1 - a3 * a3 * a3) / R);
+  } else {
+    D = sqrt(0.75 * a3 * a3 - 2 * a2 + 2 * sqrt(y1 * y1 - 4 * a0));
+    E = sqrt(0.75 * a3 * a3 - 2 * a2 - 2 * sqrt(y1 * y1 - 4 * a0));
+  }
+
+  if (!std::isnan(D)) {
+    dts.push_back(-a3 / 4 + R / 2 + D / 2);
+    dts.push_back(-a3 / 4 + R / 2 - D / 2);
+  }
+  if (!std::isnan(E)) {
+    dts.push_back(-a3 / 4 - R / 2 + E / 2);
+    dts.push_back(-a3 / 4 - R / 2 - E / 2);
+  }
 
   return dts;
 }
